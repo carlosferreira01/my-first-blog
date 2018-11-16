84ddd98eaebbfd37b8a9ee82fd4e6576c7740415
Carlos <duduferreira_01@hotmail.com>
2018-11-16 02:33:34 -0200

    Commit correct

--- blog/views.py
+++ blog/views.py
@@ -11,3 +11,9 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def about(request): 
+    return render(request, 'blog/about.html',{})
+
+def contact(request): 
+    return render(request, 'blog/contact.html',{})
